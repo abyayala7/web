44445293bafdd09bc1bc44698033171561bd91de
abyayala7 <windermartinez0000001@gmail.com>
2022-08-18 10:27:06 -0400

    Inicio

---
+++ js/footer.js
@@ -0,0 +1,19 @@
+/*document.write("<hr style=\"margin: 30pt 0pt 0pt;\">");
+document.write("<p style=\"margin: 10pt 100pt 0pt; text-align: justify; line-height: normal;\">");
+document.write("<i>©[Copyright]<\/i>");
+document.write();
+document.write("<\/p>");
+document.write('<p class="letras-footer">');
+document.write("<i>");
+document.write("Las obras publicadas en este sitio son de libre acceso para fines académicos y como un medio de difundir la");
+document.write("producción cultural y el conocimiento generados por autores universitarios o auspiciados por las universidades, pero que estos y la Editorial se reservan el derecho de autoría a todos los fines que correspondan.");
+document.write("<\/i><\/p>");
+document.write("<hr>");*/
+
+document.getElementById('footer').innerHTML="<h2>Copyright © Clínica Equilibrium</h2>";
+
+/*
+var node = document.getElementById('node-id');
+var newNode = document.createElement('p');
+newNode.appendChild(document.createTextNode('some dynamic html'));
+node.appendChild(newNode);*/
